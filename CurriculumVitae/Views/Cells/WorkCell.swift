9f38f5414dc927ceb1eefac115fd0257a8b6dd26
Carlos Saldana <saldanatic@gmail.com>
2019-08-20 20:48:19 -0500

    Create work cell and set navigation controller as root of application.

---
+++ CurriculumVitae/Views/Cells/WorkCell.swift
@@ -0,0 +1,27 @@
+//
+//  WorkCell.swift
+//  CurriculumVitae
+//
+//  Created by Apocalapsus on 20/08/19.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+import UIKit
+
+class WorkCell: UITableViewCell {
+
+    @IBOutlet weak var companyLbl: UILabel!
+    @IBOutlet weak var datesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
