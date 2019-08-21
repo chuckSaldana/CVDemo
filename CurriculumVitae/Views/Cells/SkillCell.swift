21568226d4dfb6736013baf620192bc35a9b48bb
Carlos Saldana <saldanatic@gmail.com>
2019-08-20 20:23:23 -0500

    Create skill cell.

---
+++ CurriculumVitae/Views/Cells/SkillCell.swift
@@ -0,0 +1,25 @@
+//
+//  SkillCell.swift
+//  CurriculumVitae
+//
+//  Created by Apocalapsus on 20/08/19.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+import UIKit
+
+class SkillCell: UITableViewCell {
+
+    @IBOutlet weak var nameLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
