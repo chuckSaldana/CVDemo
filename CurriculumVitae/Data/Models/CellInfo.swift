c892576e70d9f65190722abba2412f5c053fe14a
Carlos Saldana <saldanatic@gmail.com>
2019-08-21 20:42:08 -0500

    Include information objects for cells to draw content.

--- CurriculumVitae/Data/Models/CellInfo.swift
+++ CurriculumVitae/Data/Models/CellInfo.swift
@@ -19,50 +19,152 @@ enum CVCellType {
     case reference
 }
 
+// Declare a basic Cell model type
 protocol CellInfo {
     var type: CVCellType { get }
 }
 
-struct ProfileCellInfo: CellInfo {
-    var type: CVCellType
+// Declare a basic Section model type
+// This type's instances may contain 0 or more Cell info items
+protocol SectionInfo {
+    var title: String { get }
+    var items: [CellInfo] { get }
+}
+
+// In order to support selction of cells that belong to a
+// populated section
+extension SectionInfo {
+    var selectable: Bool {
+        return items.count > 0
+    }
+}
+
+// MARK: Profile
+struct ProfileCellInfo: CellInfo, SectionInfo {
+    var items: [CellInfo] = []
+    var title: String = "Profile"
+    var type: CVCellType = .profile
     var photoPath: String?
     var name: String?
     var label: String?
     var summary: String?
+    
+    init(photoPath: String?, name: String?, label: String?, summary: String?) {
+        self.photoPath = photoPath
+        self.name = name
+        self.label = label
+        self.summary = summary
+    }
 }
 
-struct ContactCellInfo: CellInfo {
+// MARK: Contact
+struct ContactCellInfo: CellInfo, SectionInfo {
+    var items: [CellInfo] = []
+    var title: String = "Contact Information"
     var type: CVCellType = .contact
     var email: String?
     var phone: String?
     var website: String?
-    var city: String?
-    var state: String?
+    var location: String?
+    
+    init(email: String?, phone: String?, website: String?, location: String?) {
+        self.email = email
+        self.phone = phone
+        self.website = website
+        self.location = location
+    }
+}
+
+// MARK: Skills
+struct SkillSection: SectionInfo {
+    var title: String = "Skills"
+    var items: [CellInfo]
+    
+    init(skills: [SkillCellInfo]) {
+        self.items = skills
+    }
 }
 
 struct SkillCellInfo: CellInfo {
     var type: CVCellType = .skill
     var name: String?
+    
+    init(name: String?) {
+        self.name = name
+    }
+}
+
+// MARK: Work experience
+struct RoleSection: SectionInfo {
+    var title: String = "Work Experience"
+    var items: [CellInfo]
+    
+    init(roles: [RoleCellInfo]) {
+        self.items = roles
+    }
 }
 
 struct RoleCellInfo: CellInfo {
     var type: CVCellType = .role
     var company: String?
-    var dateStart: Date?
-    var dateEnd: Date?
+    var dateRange: String?
+    
+    init(company: String?, dateRange: String?) {
+        self.company = company
+        self.dateRange = dateRange
+    }
+}
+
+// MARK: Education
+struct EducationSection: SectionInfo {
+    var title: String = "Education"
+    var items: [CellInfo]
+    
+    init(educationExperiences: [EducationCellInfo]) {
+        self.items = educationExperiences
+    }
 }
 
 struct EducationCellInfo: CellInfo {
     var type: CVCellType = .education
     var institution: String?
     var degree: String?
-    var dateStart: Date?
-    var dateEnd: Date?
+    var dateRange: String?
+    
+    init(institution: String?, degree: String?, dateRange: String?) {
+        self.institution = institution
+        self.degree = degree
+        self.dateRange = dateRange
+    }
+}
+
+// MARK: Interests
+struct InterestsSection: SectionInfo {
+    var title: String = "Interests"
+    var items: [CellInfo]
+    
+    init(interests: [InterestCellInfo]) {
+        self.items = interests
+    }
 }
 
 struct InterestCellInfo: CellInfo {
     var type: CVCellType = .interest
     var name: String?
+    
+    init(name: String?) {
+        self.name = name
+    }
+}
+
+// MARK: References
+struct ReferenceSection: SectionInfo {
+    var title: String = "References"
+    var items: [CellInfo]
+    
+    init(references: [ReferenceCellInfo]) {
+        self.items = references
+    }
 }
 
 struct ReferenceCellInfo: CellInfo {
@@ -70,4 +172,10 @@ struct ReferenceCellInfo: CellInfo {
     var name: String?
     var email: String?
     var phone: String?
+    
+    init(name: String?, email: String?, phone: String?) {
+        self.name = name
+        self.email = email
+        self.phone = phone
+    }
 }
