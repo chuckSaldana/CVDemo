fb74bf488f0870e57b0eff8cd150fe58124ac34b
Carlos Saldana <saldanatic@gmail.com>
2019-08-21 20:38:04 -0500

    The CVCell protocol defines same info property for all required cells in the app.

---
+++ CurriculumVitae/Views/Cells/CVCell.swift
@@ -0,0 +1,13 @@
+//
+//  CVCell.swift
+//  CurriculumVitae
+//
+//  Created by Apocalapsus on 21/08/19.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+import Foundation
+
+protocol CVCell {
+    var info: CellInfo? { get set }
+}
